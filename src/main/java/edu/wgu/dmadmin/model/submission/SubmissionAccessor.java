0a079e75da7126503d3a04ef3ee3c43382d2f444
Jessica Pamdeth <jessica.pamdeth@wgu.edu>
2018-03-30 14:52:27 -0700

    Update versions of parent POM and autoconfigure.  Update app version.

--- src/main/java/edu/wgu/dmadmin/model/submission/SubmissionAccessor.java
+++ src/main/java/edu/wgu/dmadmin/model/submission/SubmissionAccessor.java
@@ -8,6 +8,6 @@ import com.datastax.driver.mapping.annotations.Query;
 @Accessor
 public interface SubmissionAccessor {
 	
-    @Query("SELECT submission_id, task_id, attempt, student_id FROM dm.submission_by_id WHERE student_id = ? AND task_id = ? limit 1")
+    @Query("SELECT submission_id, task_id, attempt, student_id FROM dm.submission_by_id WHERE student_id = ? AND task_id = ? limit 1 ALLOW FILTERING")
     SubmissionModel getLastSubmissionByStudentAndTask(String studentId, UUID taskId);
 }
