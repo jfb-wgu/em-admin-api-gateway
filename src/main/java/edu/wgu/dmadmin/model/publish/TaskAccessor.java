b96d2171b1af346ce771921f70799bd82801102e
Jessica Pamdeth <jessica.pamdeth@wgu.edu>
2018-04-04 16:44:00 -0700

    Remove reference to task view.

--- src/main/java/edu/wgu/dmadmin/model/publish/TaskAccessor.java
+++ src/main/java/edu/wgu/dmadmin/model/publish/TaskAccessor.java
@@ -10,6 +10,6 @@ public interface TaskAccessor {
     @Query("SELECT task_id, task_name, assessment_id, assessment_code FROM dm.task_by_id")
     Result<EMATaskModel> getAllBasics();
     
-    @Query("SELECT assessment_id, assessment_code, task_id, task_name, task_order FROM dm.task_by_assessment where assessment_id = ?")
+    @Query("SELECT assessment_id, assessment_code, task_id, task_name, task_order FROM dm.task_by_id where assessment_id = ?")
     Result<EMATaskModel> getBasicTasksByAssessment(Long assessmentId);
 }
