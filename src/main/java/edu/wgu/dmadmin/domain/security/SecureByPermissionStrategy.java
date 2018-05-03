8d52eab1fd7bebddc340872bb5f4269236d32b5a
Jessica Pamdeth <jessica.pamdeth@wgu.edu>
2018-05-03 16:03:44 -0700

    Enforce employee role in AuthorizationStrategy

--- src/main/java/edu/wgu/dmadmin/domain/security/SecureByPermissionStrategy.java
+++ src/main/java/edu/wgu/dmadmin/domain/security/SecureByPermissionStrategy.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 
 import edu.wgu.common.domain.AuthzIdentityKeys;
+import edu.wgu.common.domain.Role;
 import edu.wgu.dmadmin.config.ApplicationContextHolder;
 import edu.wgu.dmadmin.model.security.UserModel;
 import edu.wgu.dmadmin.repo.CassandraRepo;
@@ -13,33 +14,36 @@ import edu.wgu.security.authz.domain.AuthorizationInfo;
 import edu.wgu.security.authz.strategy.AuthorizationStrategy;
 
 public class SecureByPermissionStrategy implements AuthorizationStrategy {
-	
+
 	@Override
 	public boolean isAuthorized(final HttpServletRequest request, final AuthorizationInfo authorizationInformation) {
-		CassandraRepo cassandraRepo = ApplicationContextHolder.getContext().getBean(CassandraRepo.class);
-		RequestBean requestBean = ApplicationContextHolder.getContext().getBean(RequestBean.class);
-
-		String userId = null;
-		Optional<UserModel> user = null;
 		Optional<AuthzIdentityKeys> identityKeys = AuthzIdentityKeys.retrieveFrom(request);
 
 		if (identityKeys.isPresent()) {
-			userId = identityKeys.get().getBannerId();
-		}
+			AuthzIdentityKeys keys = identityKeys.get();
 
-		if (userId != null) {
-			user = cassandraRepo.getUserModel(userId);
-			
-			if (user.isPresent()) {
-				cassandraRepo.updateLastLogin(userId);
-				requestBean.setUser(user.get());
-				Set<String> userPermissions = user.get().getPermissions();
-				requestBean.setAuthRoles(authorizationInformation.getRoles());
-				return authorizationInformation.getRoles().stream()
-						.filter(role -> userPermissions.contains(role))
-						.count() > 0;
+			if (keys.getRoles() == null || !keys.getRoles().contains(Role.EMPLOYEE))
+				return false;
+
+			String userId = keys.getBannerId();
+
+			if (userId != null) {
+				CassandraRepo cassandraRepo = ApplicationContextHolder.getContext().getBean(CassandraRepo.class);
+				RequestBean requestBean = ApplicationContextHolder.getContext().getBean(RequestBean.class);
+
+				Optional<UserModel> user = cassandraRepo.getUserModel(userId);
+
+				if (user.isPresent()) {
+					cassandraRepo.updateLastLogin(userId);
+					requestBean.setUser(user.get());
+					Set<String> userPermissions = user.get().getPermissions();
+					requestBean.setAuthRoles(authorizationInformation.getRoles());
+					return authorizationInformation.getRoles().stream().filter(role -> userPermissions.contains(role))
+							.count() > 0;
+				}
 			}
 		}
+
 		return false;
 	}
 }
