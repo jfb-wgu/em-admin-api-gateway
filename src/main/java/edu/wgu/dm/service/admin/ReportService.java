f4208a710fb9b4192d0d69733683eea580c8a593
Amit Shrigondekar <amit.shrigondekar@wgu.edu>
2018-08-17 14:22:43 -0600

    moved services from dm-worker to this project so they stay where they are used

---
+++ src/main/java/edu/wgu/dm/service/admin/ReportService.java
@@ -0,0 +1,34 @@
+package edu.wgu.dm.service.admin;
+
+import java.util.Date;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import edu.wgu.dm.admin.domain.report.EmaEvaluationAspectRecord;
+import edu.wgu.dm.admin.domain.report.EmaTaskRubricRecord;
+import edu.wgu.dm.dto.publish.Competency;
+import edu.wgu.dm.repository.admin.AdminRepository;
+import edu.wgu.dm.util.utils.DmUtil;
+
+@Service
+public class ReportService {
+
+    @Autowired
+    private AdminRepository adminRepo;
+
+    public List<Competency> getTaskCompetencies(Date datePublished) {
+        return adminRepo.getTaskCompetencies(DmUtil.startOfDay(datePublished));
+    }
+
+    public List<EmaEvaluationAspectRecord> getEvaluationAspects(Date startDate, Date endDate) {
+        if (startDate.after(endDate)) {
+            throw new IllegalArgumentException("Start date must be before end date.");
+        }
+        return adminRepo.getEvaluationAspects(DmUtil.startOfDay(startDate),
+                DmUtil.endOfDay(endDate));
+    }
+
+    public List<EmaTaskRubricRecord> getRubrics(Date datePublished) {
+        return adminRepo.getTaskRecords(datePublished);
+    }
+}
