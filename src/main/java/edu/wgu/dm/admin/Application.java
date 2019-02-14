f3039bde3bc679b195d5c7a6f71a1cddbeba6f12
Jessica Pamdeth <jessica.pamdeth@wgu.edu>
2019-02-14 12:33:43 -0800

    Changes suggested by Courtney.

--- src/main/java/edu/wgu/dm/admin/Application.java
+++ src/main/java/edu/wgu/dm/admin/Application.java
@@ -2,10 +2,7 @@ package edu.wgu.dm.admin;
 
 import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -13,12 +10,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import edu.wgu.boot.core.config.WGUBootApplication;
 
-@SpringBootApplication
+@WGUBootApplication
 @ComponentScan(basePackages = {"edu.wgu.dm.admin", "edu.wgu.dm.config", "edu.wgu.dm.audit", "edu.wgu.dm.util",
         "edu.wgu.dm.health", "edu.wgu.dm.security.strategy", "edu.wgu.dm.repository", "edu.wgu.dm.security.service"})
 @EnableFeignClients(basePackages = {"edu.wgu.dm.service.feign"})
-@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 @EnableAspectJAutoProxy
 @EnableJpaAuditing
 @EnableCaching
