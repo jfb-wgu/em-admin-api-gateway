73e7c30b5fcfdc0893a83564a2e7c5e5e243cb09
Jessica Pamdeth <jessica.pamdeth@wgu.edu>
2019-02-14 11:48:23 -0800

    Remove work from read/write replica splitting.

--- src/main/java/edu/wgu/dm/admin/Application.java
+++ src/main/java/edu/wgu/dm/admin/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
@@ -11,15 +12,18 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"edu.wgu.dm.admin", "edu.wgu.dm.config", "edu.wgu.dm.audit", "edu.wgu.dm.util",
         "edu.wgu.dm.health", "edu.wgu.dm.security.strategy", "edu.wgu.dm.repository", "edu.wgu.dm.security.service"})
 @EnableFeignClients(basePackages = {"edu.wgu.dm.service.feign"})
-@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 @EnableAspectJAutoProxy
 @EnableJpaAuditing
 @EnableCaching
+@EntityScan({"edu.wgu.dm.entity", "edu.wgu.dm.view"})
+@EnableJpaRepositories(basePackages = {"edu.wgu.dm.repo", "edu.wgu.dm.view"})
 public class Application {
 
     /**
